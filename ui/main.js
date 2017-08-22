665869e9bf9c53b0f6a89a1cdb769527cf8a9f45
Sowmyashreep <31010344+Sowmyashreep@users.noreply.github.com>
2017-08-22 22:08:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var button=document.getElementById('counter');
 button.onclick=function()
 {
     
-    var requesrt=new XMLHttpRequest();
+    var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
       if(request.readystate===XMLHttpRequest.DONE){
           if(request.status===200){
