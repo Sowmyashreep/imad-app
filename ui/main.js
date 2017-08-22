3dbfce2d26ca39acb47666441f2cf96ce89a85fb
Sowmyashreep <31010344+Sowmyashreep@users.noreply.github.com>
2017-08-22 21:31:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,10 @@
-console.log('Loaded!');
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function(){
-    var interval= setInterval(moveRight,50);
+var button=document.getElementById('counter');
+button.onclick=function()
+{
     
+    var counter=0;
+     counter=counter+1;
+     var span=document.getElementById('count');
+     span.InnerHTML=counter.toString();
     
 };
